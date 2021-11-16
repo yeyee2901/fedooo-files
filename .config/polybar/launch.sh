ba39f2c24ed745a57c08d49ced55c37ea304385a
yeyee2901 <gabriel777sh@gmail.com>
2021-11-16 19:13:19 +0700

    ADD: multi monitor capabilities for i3 and polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-# Terminate already running bar instances
 killall -q polybar
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar main 2>&1 | tee -a /tmp/polybar1.log & disown
+# echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+
+if type "xrandr"; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar --reload main &
+    done
+else
+    polybar main 2>&1 | tee -a /tmp/polybar1.log & disown
+fi
