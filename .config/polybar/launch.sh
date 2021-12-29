65ebf0cb1284f6b43b42ac41c86fc8e8148edae9
yeyee2901 <gabriel777sh@gmail.com>
2021-12-29 16:30:09 +0700

    EDIT: now workspace uses icons

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -2,8 +2,6 @@
 
 killall -q polybar
 
-# echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-
 if type "xrandr"; then
     for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
         MONITOR=$m polybar --reload main &
